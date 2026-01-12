23ae81b6900def34f8f5671d2429412b37fdb41f
yjkh17 <yj.kh17@gmail.com>
2026-01-12 09:02:48 +0300

    Initial Commit

---
+++ Water/WaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterApp.swift
+//  Water
+//
+//  Created by Yousef Jawdat on 12/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct WaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
